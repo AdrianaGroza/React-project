7c81600c4295bce170dfd007ca5d7138d44115dc
Groza Zita <adi_groza@yahoo.com>
2023-02-06 21:36:22 +0200

    added tailwind and data for the project

---
+++ src/PerfumesData.js
@@ -0,0 +1,96 @@
+const PERFUMES_DATA = [
+    
+    { 
+        id: 1, 
+        perfumeName: 'Black Opium', 
+        perfumeBrand: 'Yves Saint Laurent', 
+        quantity: 50, 
+        unitOfMeasure: 'ml' , 
+        range: 'for her', 
+        conc: 'Eau de Parfum' 
+    },
+    { 
+        id: 2, 
+        perfumeName: 'La Vie est Belle', 
+        perfumeBrand: 'Lancôme', 
+        quantity: 75, 
+        unitOfMeasure: 'ml', 
+        range: 'for her', 
+        conc: 'Eau de Parfum' 
+    },
+    { 
+        id: 3, 
+        perfumeName: 'Coco Mademoiselle', 
+        perfumeBrand: 'Chanel', 
+        quantity: 35, 
+        unitOfMeasure: 'ml', 
+        range: 'for her', 
+        conc: 'Eau de Parfum' 
+    },
+    { 
+        id: 4, 
+        perfumeName: 'Angel', 
+        perfumeBrand: 'Thierry Mugler', 
+        quantity: 50, 
+        unitOfMeasure: 'ml', 
+        range: 'for her', 
+        conc: 'Eau de Parfum' 
+    },
+    { 
+        id: 5, 
+        perfumeName: 'Daisy', 
+        perfumeBrand: 'Marc Jacobs', 
+        quantity: 30, 
+        unitOfMeasure: 'ml', 
+        range: 'for her', 
+        conc: 'Eau de Toilette' 
+    },
+    { 
+        id: 6, 
+        perfumeName: 'Light Blue', 
+        perfumeBrand: 'Dolce & Gabbana', 
+        quantity: 50, 
+        unitOfMeasure: 'ml', 
+        range: 'for him', 
+        conc: 'Eau de Toilette' 
+    },
+    { 
+        id: 7, 
+        perfumeName: 'Aqua di Gio', 
+        perfumeBrand: 'Giorgio Armani', 
+        quantity: 60, 
+        unitOfMeasure: 'ml', 
+        range: 'for him', 
+        conc: 'Eau de Toilette' 
+    },
+    { 
+        id: 8, 
+        perfumeName: 'Invictus', 
+        perfumeBrand: 'Paco Rabanne', 
+        quantity: 75, 
+        unitOfMeasure: 'ml', 
+        range: 'for him', 
+        conc: 'Eau de Toilette' 
+    },
+    { 
+        id: 9, 
+        perfumeName: 'Bleu de Chanel', 
+        perfumeBrand: 'Chanel', 
+        quantity: 100, 
+        unitOfMeasure: 'ml', 
+        range: 'for him', 
+        conc: 'Eau de Toilette' 
+    },
+    { 
+        id: 10, 
+        perfumeName: '1 Million', 
+        perfumeBrand: 'Paco Rabanne', 
+        quantity: 80, 
+        unitOfMeasure: 'ml', 
+        range: 'for him', 
+        conc: 'Eau de Toilette' 
+    }
+
+];
+
+export default PERFUMES_DATA;
